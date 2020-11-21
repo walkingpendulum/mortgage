41f8e691fc61f9d4c1128635c830badcb65bb311
Oleg Yunin <oayunin@gmail.com>
2020-11-21 20:26:19 +0300

    first commit

---
+++ mortgage.py
@@ -0,0 +1,61 @@
+import json
+from itertools import count
+
+import texttable
+
+
+def read_config():
+    with open('config.json') as f:
+        config = json.load(f)
+
+    return config
+
+
+def infer_mortgage_numbers(body, interest, payment):
+    payments = []
+    interests_payed = []
+    body_per_month = []
+
+    while body > 0:
+        interest_to_pay = body * (interest / 100 / 12)
+        payment_after_interest = payment - interest_to_pay
+        body -= payment_after_interest
+
+        payments.append(payment - interest)
+        interests_payed.append(interest_to_pay)
+        body_per_month.append(max(body, 0))
+
+    return payments, interests_payed, body_per_month
+
+
+if __name__ == '__main__':
+    config = read_config()
+    interest_percentage = config.get('interest_percentage', 10)
+    mortgage_body_initial = config.get('mortgage_body_initial', 100)
+    monthly_payment = config.get('monthly_payment', 10)
+
+    data = []
+
+    for interest_p in count(interest_percentage, -0.2):
+        if interest_p < 5:
+            break
+
+        payments, interests_payed, body_per_month = infer_mortgage_numbers(
+            body=mortgage_body_initial,
+            interest=interest_p,
+            payment=monthly_payment,
+        )
+
+        duration = len(body_per_month)
+        overhead = monthly_payment * (duration - 1) + (body_per_month[-2]) - mortgage_body_initial
+        data.append([f'{interest_p:.2f}', duration, f'{int(overhead / 1e3)}'])
+
+    data.insert(0, ["Процент", "Срок кредита", "Переплата, тысячи"])
+    data = list(zip(*data))
+    table = texttable.Texttable(max_width=240)
+    table.set_cols_dtype(['t'] * len(data[0]))
+    table.add_rows(data)
+
+    print(f"Сумма кредита: {mortgage_body_initial}")
+    print(f"Размер выплат: {monthly_payment}")
+    print(table.draw())
